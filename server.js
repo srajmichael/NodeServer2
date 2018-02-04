f6a7fb7c6448cff4d91bcd7f7a39cd43eca0febd
Michael Sraj <michaelsrajjr@gmail.com>
2018-02-04 09:27:38 -0500

    added a projects.hbs page to server and updated header

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about',(req,res)=>{
   });
 });
 
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  });
+});
+
 
 app.get('/derp/morederp',(req,res)=>{
   res.send('<h1>DERP DERP</h1>');
